e32551a2705a1a040cfd7fd441ec55d6d5fea95c
Takumikimura1899 <kimura141899@gmail.com>
2024-04-28 15:20:58 +0900

    resolver prismaClientを使った形に修正

--- src/task/task.resolver.ts
+++ src/task/task.resolver.ts
@@ -1,21 +1,24 @@
+import { Task } from '@prisma/client';
 import { CreateTaskInput } from './dto/createTask.input';
-import { Task } from './models/task.model';
+import { Task as TaskModel } from './models/task.model';
 import { TaskService } from './task.service';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class TaskResolver {
-  constructor(private readonly TaskService: TaskService) {}
+  constructor(private readonly taskService: TaskService) {}
 
   // @Query()デコレータを使用して、getTasksメソッドをGraphQLのクエリとして公開する。
-  @Query(() => [Task], { nullable: 'items' })
-  getTasks(): Task[] {
-    return this.TaskService.getTasks();
+  @Query(() => [TaskModel], { nullable: 'items' })
+  async getTasks(): Promise<Task[]> {
+    return await this.taskService.getTasks();
   }
 
   // @Mutation()デコレータを使用して、createTaskメソッドをGraphQLのミューテーションとして公開する。
-  @Mutation(() => Task)
-  createTask(@Args('createTaskInput') createTaskInput: CreateTaskInput): Task {
-    return this.TaskService.createTask(createTaskInput);
+  @Mutation(() => TaskModel)
+  async createTask(
+    @Args('createTaskInput') createTaskInput: CreateTaskInput,
+  ): Promise<Task> {
+    return await this.taskService.createTask(createTaskInput);
   }
 }
